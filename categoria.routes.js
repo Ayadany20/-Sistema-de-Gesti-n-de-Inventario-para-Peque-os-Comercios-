22deadc8bc32ee8426d53ea49f837aa8004da26b
JackDua99 <graciela.duartetiznado@cesunbc.edu.mx>
2025-07-26 16:46:32 -0700

    Create categoria.routes.js
    Apartado de categoria en rutas

---
+++ categoria.routes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const controller = require('../controllers/categoria.controller');
+
+router.get('/', controller.getCategorias);
+router.get('/:id', controller.getCategoriaById);
+router.post('/', controller.createCategoria);
+router.put('/:id', controller.updateCategoria);
+router.delete('/:id', controller.deleteCategoria);
+
+module.exports = router;
