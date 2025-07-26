7b5acbc88da2de87213d957afe7eb33a4a6b89e4
JackDua99 <graciela.duartetiznado@cesunbc.edu.mx>
2025-07-26 16:48:17 -0700

    Create proveedor.routes.js
    apartado de proveedor en el area de rutas

---
+++ proveedor.routes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const controller = require('../controllers/proveedor.controller');
+
+router.get('/', controller.getProveedores);
+router.get('/:id', controller.getProveedorById);
+router.post('/', controller.createProveedor);
+router.put('/:id', controller.updateProveedor);
+router.delete('/:id', controller.deleteProveedor);
+
+module.exports = router;
