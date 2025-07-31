cc9059f80379a9048c55ebab1508fb73aa36d492
JackDua99 <graciela.duartetiznado@cesunbc.edu.mx>
2025-07-31 11:05:32 -0700

    Update movimiento.routes.js

--- movimiento.routes.js
+++ movimiento.routes.js
@@ -3,8 +3,6 @@ const router = express.Router();
 const controller = require('../controllers/movimiento.controller');
 
 router.get('/', controller.getMovimientos);
-router.get('/:id', controller.getMovimientoById); // opcional
 router.post('/', controller.createMovimiento);
-// normalmente no se actualizan o eliminan movimientos
 
 module.exports = router;
