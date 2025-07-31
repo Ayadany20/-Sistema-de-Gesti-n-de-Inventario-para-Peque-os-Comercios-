b8a5fd4200046840ce7aa327fed421e67553df30
JackDua99 <graciela.duartetiznado@cesunbc.edu.mx>
2025-07-31 11:04:39 -0700

    Update movimiento.crontroller.js

--- movimiento.crontroller.js
+++ movimiento.crontroller.js
@@ -1,11 +1,21 @@
 const { Movimiento } = require('../models');
 
 exports.getMovimientos = async (req, res) => {
-  const movimientos = await Movimiento.findAll();
-  res.json(movimientos);
+  try {
+    const movimientos = await Movimiento.findAll();
+    res.json(movimientos);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ mensaje: 'Error al obtener movimientos' });
+  }
 };
 
 exports.createMovimiento = async (req, res) => {
-  const nuevo = await Movimiento.create(req.body);
-  res.status(201).json(nuevo);
+  try {
+    const nuevo = await Movimiento.create(req.body);
+    res.status(201).json(nuevo);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ mensaje: 'Error al crear movimiento' });
+  }
 };
