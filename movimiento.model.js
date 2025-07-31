f0b273852a7aad0f709d247a97b9ce18670f23c9
JackDua99 <graciela.duartetiznado@cesunbc.edu.mx>
2025-07-31 11:05:09 -0700

    Update movimiento.model.js

--- movimiento.model.js
+++ movimiento.model.js
@@ -2,13 +2,13 @@ const { DataTypes } = require('sequelize');
 const sequelize = require('../config/db');
 
 const Movimiento = sequelize.define('movimiento', {
-  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-  tipo: DataTypes.ENUM('entrada', 'salida'), // Ajusta los valores reales si difieren
-  producto_id: DataTypes.INTEGER,
-  proveedor_id: DataTypes.INTEGER,
-  cantidad: DataTypes.INTEGER,
-  precio: DataTypes.DECIMAL,
-  fecha: DataTypes.DATE
+  movimientoId: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  tipomovimiento: { type: DataTypes.STRING(100), allowNull: false },
+  cantidad: { type: DataTypes.INTEGER, allowNull: false },
+  fecha: { type: DataTypes.DATE, allowNull: false },
+  precioProducto: { type: DataTypes.DECIMAL(10, 2), allowNull: true },
+  proveedorId: { type: DataTypes.INTEGER, allowNull: true },
+  productoId: { type: DataTypes.INTEGER, allowNull: true }
 }, {
   tableName: 'movimiento',
   timestamps: false
