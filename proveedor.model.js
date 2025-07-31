ca6d1b0721190709a6c1aa7494ce69e8a536b7f4
JackDua99 <graciela.duartetiznado@cesunbc.edu.mx>
2025-07-31 11:11:00 -0700

    Update proveedor.model.js

--- proveedor.model.js
+++ proveedor.model.js
@@ -2,10 +2,10 @@ const { DataTypes } = require('sequelize');
 const sequelize = require('../config/db');
 
 const Proveedor = sequelize.define('proveedor', {
-  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-  name: DataTypes.STRING,
-  contacto: DataTypes.STRING,
-  direccion: DataTypes.STRING
+  proveedorId: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  nameproveedor: { type: DataTypes.STRING(100), allowNull: false },
+  contacto: { type: DataTypes.STRING(100), allowNull: false },
+  direccion: { type: DataTypes.STRING(150), allowNull: false }
 }, {
   tableName: 'proveedor',
   timestamps: false
